21b683c77f8565623fe1d6b34a5126969d804626
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2023-10-30 01:17:43 -0600

    TERMINADO FINAL PUNTO EXTRA

--- codigo.R
+++ codigo.R
@@ -1,6 +1,7 @@
 library(copula)
 library(lattice)
 library(actuar)
+library(plotly)
 
 graficaDensidadCopulas <- function(myCopulaP,myCopulaN, n=10000, seed=191654){
 
@@ -86,13 +87,53 @@ z2 <- matrix(dens2$estimate, nrow = length(x2), ncol = length(y2))
 
 
 # Crear gráficos interactivos con plotly
-p <- plot_ly(x = ~x, y = ~y, z = ~z) %>% add_surface()
-p2 <- plot_ly(x = ~x2, y = ~y2, z = ~z2) %>% add_surface()
+p <- plot_ly(x = ~x, y = ~y, z = ~z) %>% add_surface() %>%
+  layout(scene = list(xaxis = list(range = c(NA, 100)),
+                      yaxis = list(range = c(NA, 100)),
+                      zaxis = list(range = c(NA, 100))))
+
+p2 <- plot_ly(x = ~x2, y = ~y2, z = ~z2) %>% add_surface() %>%
+  layout(scene = list(xaxis = list(range = c(NA, 100)),
+                      yaxis = list(range = c(NA, 100)),
+                      zaxis = list(range = c(NA, 100))))
+
 
 return(list(p,p2))
 }
 
 graficaDependenciaCopulas2D <- function(myCopulaP, myCopulaN, n = 10000, seed = 123, alpha = 1, pareto_params1 = 1, pareto_params2 = 1) {
+  # Cargar librerías necesarias
+  library(copula)
+  library(ggplot2)
+
+  # Configurar la semilla para reproducibilidad
+  set.seed(seed)
+
+  # Generar muestras de la cópula
+  samplesP <- rCopula(n, myCopulaP) # Caso de dependencia positiva
+  samplesN <- rCopula(n, myCopulaN) # Caso de dependencia negativa
+
+  # Convertir las muestras a distribuciones Pareto
+  # Asumiendo que qpareto es una función adecuada para generar cuantiles de Pareto
+  samplesP <- apply(samplesP, 2, function(x) qexp(x, rate = params1))
+  samplesN <- apply(samplesN, 2, function(x) qexp(x, rate = params2))
+
+  # Crear data frames para gráficos
+  dfP <- data.frame(X = samplesP[, 1], Y = samplesP[, 2], Case = 'Positive Dependence')
+  dfN <- data.frame(X = samplesN[, 1], Y = samplesN[, 2], Case = 'Negative Dependence')
+  df <- rbind(dfP, dfN)
+
+  # Gráfico de dispersión con ggplot2
+  p <- ggplot(df, aes(x = X, y = Y, color = Case)) +
+    geom_point(alpha = 0.5) +
+    theme_minimal() +
+    ggtitle("Dependence between Variables Using Copulas") +
+    labs(x = "Variable 1", y = "Variable 2", color = "Dependence Type") 
+  return(p)
+}
+
+
+graficaDependenciaCopulas2D2 <- function(myCopulaP, myCopulaN, n = 10000, seed = 123, alpha = 1, pareto_params1 = 1, pareto_params2 = 1) {
   # Cargar librerías necesarias
   library(copula)
   library(ggplot2)
@@ -119,7 +160,8 @@ graficaDependenciaCopulas2D <- function(myCopulaP, myCopulaN, n = 10000, seed = 
     geom_point(alpha = 0.5) +
     theme_minimal() +
     ggtitle("Dependence between Variables Using Copulas") +
-    labs(x = "Variable 1", y = "Variable 2", color = "Dependence Type")
+    labs(x = "Variable 1", y = "Variable 2", color = "Dependence Type")+
+    coord_cartesian(xlim = c(NA, 100), ylim = c(NA, 100)) # Límites para los ejes
 
   return(p)
 }
